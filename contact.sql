a7c113e806fc5761f9671adf6d6a712dab8227b2
Nadina <123557352+Nadina999@users.noreply.github.com>
2024-10-29 12:56:01 +0100

    Update contact.sql

--- contact.sql
+++ contact.sql
@@ -4,9 +4,9 @@ USE contact;
 
 CREATE TABLE poruke (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    ime_prezime VARCHAR(100) NOT NULL,
-    broj_telefona VARCHAR(20),
-    email VARCHAR(100),
+    ime_prezime NCHAR(100) NOT NULL,
+    broj_telefona INT(20),
+    email NCHAR(100),
     poruka TEXT,
     datum TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
